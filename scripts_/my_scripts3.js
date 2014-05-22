87d9578a3f37478816fb74715056e7f52d505297
Emily Schleiner <cordial.emily@gmail.com>
2014-05-21 19:09:01 -0700

    moving images to files

--- scripts_/my_scripts3.js
+++ scripts_/my_scripts3.js
@@ -14,7 +14,8 @@
 		$('#contentVideo').hide().slideDown(1000);
 	  });
 	});
-
+					   
+    
 <!-- this javascript makes all links with classname "newWin" open in a new window -->				   
 						   					   
 window.onload = newWinLinks;
@@ -39,33 +40,3 @@ window.onload = newWinLinks;
         $('.text_etc').hide().slideDown(1000);
       });
     });
-	
-<!--This makes images larger when you hover over them -->	
-
-$(document).ready(function() {
-	var cont_left = $("#container").position().left;
-	$("a img").hover(function() {
-		// hover in
-		$(this).parent().parent().css("z-index", 1);
-		$(this).animate({
-			height: "140",
-			width: "140",
-			left: "-=10",
-			top: "-=10"
-		}, "fast");
-	}, function() {
-        // hover out
-        $(this).parent().parent().css("z-index", 0);
-        $(this).animate({
-            height: "130",
-            width: "130",
-            left: "+=10",
-            top: "+=10"
-        }, "fast");
-    });
-        
-    $(".img").each(function(index) {
-    	var left = (index * 160) + cont_left;
-        $(this).css("left", left + "px");
-    });
-});
