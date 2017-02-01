bd08472f3a8bba4f60053c61a773ddd053a20573
Emily Schleiner <cordial.emily@gmail.com>
2017-01-31 21:00:33 -0500

    optomizing js for portfolio

--- js/custom.js
+++ js/custom.js
@@ -19,41 +19,38 @@ var reply_click = function()
     var newid = this.id;
 
     function testOutIds () {
-      console.log("reading function" + newid);
+      
       // add repeat code when Type error is resolved
-      console.log("Welcome to my site. If you have questions, you can email me at cordial.emily@gmail.com")
       var oldtitle = document.getElementById(newid).getElementsByClassName("webtitle")[0].innerHTML;
       var x = document.getElementById("plansModalLabel").innerHTML = oldtitle;
+      // unique description for modal body
       var olddescr = document.getElementById(newid).getElementsByClassName("site-description")[0].innerHTML;
       var newdescr = document.getElementById("modalBody").innerHTML = olddescr;
+      // unique hrefs for 'learn more button'
+      var learnbtnlink = document.getElementById(newid).getElementsByClassName("learnbtnlink")[0];
+      var combobtn = '<a href="' + learnbtnlink.innerHTML + '" ><button type="button" class="btn btn-primary">Visit the site</button></a>';
+
+      document.getElementById("learnmore").innerHTML = combobtn;
     }
 
 
     if (newid == "b1") {
       testOutIds();
-      //console.log(oldtitle);
-      //console.log(x[0]);
-      // console.log("Welcome to my site. If you have questions, you can email me at cordial.emily@gmail.com")
-      // var oldtitle = document.getElementById(newid).getElementsByClassName("webtitle")[0].innerHTML;
-      // var x = document.getElementById("plansModalLabel").innerHTML = oldtitle;
-      // var olddescr = document.getElementById(newid).getElementsByClassName("site-description")[0].innerHTML;
-      // var newdescr = document.getElementById("modalBody").innerHTML = olddescr;
     }
     if (newid == "b2") {
       testOutIds();
-      // var oldtitle = document.getElementById(newid).getElementsByClassName("webtitle")[0].innerHTML;
-      // var x = document.getElementById("plansModalLabel").innerHTML = oldtitle;
-      // var olddescr = document.getElementById(newid).getElementsByClassName("site-description")[0].innerHTML;
-      // var newdescr = document.getElementById("modalBody").innerHTML = olddescr;
     }
     if (newid == "b3") {
-      var oldtitle = document.getElementById(newid).getElementsByClassName("webtitle")[0].innerHTML;
-      var x = document.getElementById("plansModalLabel").innerHTML = oldtitle;
-      var olddescr = document.getElementById(newid).getElementsByClassName("site-description")[0].innerHTML;
-      var newdescr = document.getElementById("modalBody").innerHTML = olddescr;
+      testOutIds();
     }
 
 }
-document.getElementById('b1').onclick = reply_click;
+/*document.getElementById('b1').onclick = reply_click;
 document.getElementById('b2').onclick = reply_click;
-document.getElementById('b3').onclick = reply_click;
+document.getElementById('b3').onclick = reply_click;*/
+
+/*document.getElementsByClassName('web')[1].onclick = reply_click;*/
+var elems = document.getElementsByClassName("web");
+for(var i = 0; i<elems.length; i++ ){
+  elems[i].onclick = reply_click;
+}
