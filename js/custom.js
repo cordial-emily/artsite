5da3e2e87bec32d64b43fb7fe4d4fc0284518078
Emily Schleiner <cordial.emily@gmail.com>
2017-04-12 16:09:34 -0700

    edit js comments

--- js/custom.js
+++ js/custom.js
@@ -14,25 +14,28 @@ jQuery(".thumbnail_").mouseover(mouseOverEffect).mouseout(mouseOutEffect);
 
 /* On CV page, Modal Pop-ups for Web portfolio ************************/
 
+// This function funnels text from html to bootstrap's modal,
+// ...making it so that I can type out bootstrap's modal once instead of 14 times.
 var reply_click = function()
 {
-    // each button has a numerical id like "b1, b2", etc
-    // when a button is clicked, the id is stored in this variable
+    // Each button has a numerical id like "b1, b2", etc.
+    // When a button is clicked, its id is stored in this variable.
     var newid = this.id;
 
-    // here is a function funnels text from my html to bootstrap's modal once,
-    // making it so that I can type out bootstrap's modal once instead of 14 times.
     function testOutIds () {
 
-      // this selects elements, taking the elements' text contents and attaching them to ids
+      // This section selects elements, taking the webtitle element's innerHTML and inserting it into plansModalLabel.
       var oldtitle = document.getElementById(newid).getElementsByClassName("webtitle")[0].innerHTML;
       var x = document.getElementById("plansModalLabel").innerHTML = oldtitle;
-      // same for the modal body
+
+      // This section selects elements, taking the site-description element's innerHTML and inserting it into modalBody.
       var olddescr = document.getElementById(newid).getElementsByClassName("site-description")[0].innerHTML;
       var newdescr = document.getElementById("modalBody").innerHTML = olddescr;
-      // same for the 'learn more button'
+
+      // This section selects the 'learn more button'.
       var learnbtnlink = document.getElementById(newid).getElementsByClassName("learnbtnlink")[0];
-      // this adds a live link to the 'learn more button'
+
+      // This adds a link to the 'learn more button'.
       var combobtn = '<a href="' + learnbtnlink.innerHTML + '" ><button type="button" class="btn btn-primary">Visit the site</button></a>';
 
       document.getElementById("learnmore").innerHTML = combobtn;
@@ -42,13 +45,15 @@ var reply_click = function()
 
 }
 
-/* This forloop cycles through each class clicked and runs the function 'reply_click' */
 var elems = document.getElementsByClassName("web");
+
+/* Assigning the 'reply_click' function as an event handler. */
 for(var i = 0; i<elems.length; i++ ){
   elems[i].onclick = reply_click;
 }
 
-/* Print a hello message in the console ************************************************/
+/* This print a hello message in the console. (Don't forget to check. :) ) ************************************************/
+
 function sayHelloInTheConsole() {
     console.log("Hey there!\nThanks for checking my console.\n\nIf you'd like to see some JS I've written, go to: http://cordial-emily.com/js/custom.js. \n\nPlease contact me with questions, comments, and work opportunities. Thanks! \n\nEmail: Cordial.Emily@gmail.com");
 }
